f02d4218f5b411fef205b833503e733e09dcd4dc
YahyaMallouli <95717583+YahyaMallouli@users.noreply.github.com>
2023-12-07 22:55:25 +0100

    Add files via upload

---
+++ MicroImp/src/test/java/com/example/microimp/MicroImpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microimp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroImpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
